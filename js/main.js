bc8ea41baaa67dfc9b3a74a5ec05a19e48468882
Leandro <leandrocarlospires@gmail.com>
2021-02-24 20:33:40 -0300

    Programação para internet com JS. Oferecido pela Digital Innovation One

---
+++ js/main.js
@@ -0,0 +1,136 @@
+
+
+/*
+function clicou(){
+   document.getElementById("agradecimento").innerHTML = "<b> Obrigado por clicar </b>"
+
+   //console.log(document.getElementById("agradecimento"))
+}
+
+function redirecionar(){
+   window.open("https://www.google.com/") //abre em outra janela
+    window.location.href = "https://www.google.com/" // Abre na mesma janela
+}
+
+function trocar(elemento){
+   // document.getElementById("mousemove").innerHTML = "obrigado por passar o mouse"
+   //alert("trocar texto")
+    elemento.innerHTML =  "obrigado por passar o mouse"
+}
+
+function voltar(elemento){
+   // document.getElementById("mousemove").innerHTML = "Passe o mouse aqui"
+    elemento.innerHTML = "Passe o mouse aqui"
+}
+
+function load(){
+    alert("pagina carregada")
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
+
+*/
+/*
+function soma(n1, n2) {
+    return n1 + n2;
+}
+*/
+
+
+/*
+function validaIdade(idade) {
+    var validar
+    if (idade >= 18) {
+        validar = true
+    } else {
+        validar = false
+    }
+return validar
+}
+
+var idade = prompt("Qual sua idade")
+console.log(validaIdade( idade))
+*/
+
+/*
+ function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+ }
+ alert(setReplace("Vai Japão", " Japão", " Brasil "))
+ */
+//alert(soma(5, 10))
+
+/*
+var d = new Date();
+alert(d.getMonth()+1);
+alert(d.getMinutes());
+alert(d.getDay());
+alert(d.getHours());
+*/
+
+/*
+var count
+for(count = 0; count < 5; count++){
+    console.log(count)
+}
+    */
+
+/*
+var count = 0
+while(count <= 5){
+    console.log(count)
+    count ++
+}
+*/
+
+
+/*
+var idade = prompt("qual sua idade")//Vai exibir uma pergunta e guarda o valor na variavel
+
+if(idade> 18){
+    alert("Maior de idade")
+
+}else{
+    alert("Menor de idade")
+}
+
+*/
+
+
+
+
+
+/*
+var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+console.log(frutas)
+alert (frutas[1].nome)
+*/
+
+//var lista =["maça", "pera", "laranja"];
+//lista.push("uva")  
+//lista.pop("pera")/**Retira o ultimo elemento  */
+
+//console.log(lista)
+//alert(lista[1])
+//console.log(lista.length)/**Tamanho da lista */
+//console.log(lista.reverse())/**Lista reversa */
+//console.log(lista.toString())/**mostra como uma String */
+//console.log(lista.join(" - "))/**colocou um traço */
+
+
+
+
+//var nome = "Leandro P"
+//var idade = 25
+//var idade2 = 10
+//var frase = "Japão é o melhor time do mundo"
+//alert( nome + " tem " + idade + " anos");
+//alert(idade+idade2)
+//console.log(nome)
+//console.log(idade + idade2)
+//console.log(frase.replace("Japão", "Brasil"))
+//alert(frase.replace("Japão", "Brasil"))
+//console.log(frase.toUpperCase())
+//console.log(frase.toLowerCase())
